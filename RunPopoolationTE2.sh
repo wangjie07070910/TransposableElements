325599ed7a03a48c876c9e9516a44845fae12bd4
Robin Burns <burnsro@tcd.ie>
2022-04-04 15:51:41 +0100

    Update RunPopoolationTE2.sh

--- RunPopoolationTE2.sh
+++ RunPopoolationTE2.sh
@@ -11,19 +11,17 @@
 ml bwa/0.7.17-foss-2018b
 ml samtools/1.9-foss-2018b
 ml spark/2.4.0-hadoop-2.7-java-1.8
-ref=lowcomplexity
-#refgenome=/groups/nordborg/projects/transposons/Robin/001TransposableElements/001Popte2/002negativecontrol/Col0_masked400bpsingletonexons_nootherblast.fasta
-out=/scratch-cbe/users/robin.burns/Popte2/${ref}
-refgenome='/scratch-cbe/users/robin.burns/Popte2/genome/8242.final.TEsplastidmasked_400bpnoblasthit_exonsmasked.Chr1to5.exonsPopte2.fasta'
-raw='/groups/nordborg/projects/transposons/Robin/001TransposableElements/001Popte2/005rawreads/PCRfreeIllumina_fastq'
-acc=6046_pcrfree
-#samples=$raw/'myacc'
-#export l=$SLURM_ARRAY_TASK_ID\p
-#line=`sed -n $l $samples`
-#acc=`echo $line | cut -d ' ' -f1`
-#echo $acc
-#bamfile=`echo $line | cut -d ' ' -f2`
-#echo $bamfile
+
+refgenome=Popte2_editedgenome.fasta
+out=outdir
+
+raw='location/of/fastq/files'
+
+samples=$raw/'myacc'
+export l=$SLURM_ARRAY_TASK_ID\p
+line=`sed -n $l $samples`
+acc=`echo $line | cut -d ' ' -f1`
+echo $acc
 
 
 CONDA_ENVS_PATH='/groups/nordborg/projects/suecica/005scripts/001Software'
@@ -32,56 +30,54 @@ cd /groups/nordborg/projects/suecica/005scripts/001Software
 ml anaconda3/2019.03
 source activate RobinCondaSCRATCH3/
 
-#gunzip $raw/$acc.1.fastq.gz
-#gunzip $raw/$acc.2.fastq.gz
-#cd $out
-#prinseq-lite.pl -fastq $raw/${acc}.1.fastq -fastq2 $raw/${acc}.2.fastq -out_good ${acc} -out_format 3 -lc_method entropy -lc_threshold 70
+gunzip $raw/$acc.1.fastq.gz
+gunzip $raw/$acc.2.fastq.gz
+cd $out
+#Remove low complex reads
+prinseq-lite.pl -fastq $raw/${acc}.1.fastq -fastq2 $raw/${acc}.2.fastq -out_good ${acc} -out_format 3 -lc_method entropy -lc_threshold 70
 
-#bwa mem -t 16 -M -U 15 -R '@RG\tID:'$acc'\tSM:'$acc'\tPL:Illumina\tLB:'$acc $refgenome $out/${acc}_1.fastq $out/${acc}_2.fastq > $out/$acc.sam
+bwa mem -t 16 -M -U 15 -R '@RG\tID:'$acc'\tSM:'$acc'\tPL:Illumina\tLB:'$acc $refgenome $out/${acc}_1.fastq $out/${acc}_2.fastq > $out/$acc.sam
 
-#samtools view -@ 16  -bh -t $refgenome.fai -o $out/$acc.bam $out/$acc.sam
-#samtools sort -@ 16 -o $out/$acc.sort.bam $out/$acc.bam
-#samtools index $out/$acc.sort.bam
+samtools view -@ 16  -bh -t $refgenome.fai -o $out/$acc.bam $out/$acc.sam
+samtools sort -@ 16 -o $out/$acc.sort.bam $out/$acc.bam
+samtools index $out/$acc.sort.bam
 
-#ml picard/2.18.27-java-1.8
-#java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
-#	      I=$out/$acc.sort.bam \
-#	      O=$out/$acc.rmdup.bam \
-#	      M=$out/${acc}_dup_metrics.txt
-
-
-#samtools index $out/$acc.rmdup.bam
 #this step isnt needed for pcrfree
+ml picard/2.18.27-java-1.8
+java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
+        I=$out/$acc.sort.bam \
+	      O=$out/$acc.rmdup.bam \
+	      M=$out/${acc}_dup_metrics.txt\ 
+        REMOVDE_DUPLICATES=true
+
+
+samtools index $out/$acc.rmdup.bam
+
+#Remove non unique reads
 
 #ILLUM
-#samtools view -F 256 -q 20 -h -b $out/$acc.rmdup.bam > $out/$acc.filt.bam
+samtools view -F 256 -q 20 -h -b $out/$acc.rmdup.bam > $out/$acc.filt.bam
+samtools index $out/$acc.filt.bam
 
 #PCR
-#samtools view -F 256 -q 20 -h -b $out/$acc.sort.bam > $out/$acc.filt.bam
-#samtools index $out/$acc.filt.bam
-#be sure to switch back to acc
+samtools view -F 256 -q 20 -h -b $out/$acc.sort.bam > $out/$acc.filt.bam
+samtools index $out/$acc.filt.bam
 
-#samtools index ${bamfile}
-#samtools index $out/$acc.filt2.bam
 
-#hier='/groups/nordborg/projects/transposons/Robin/001TransposableElements/001Popte2/002negativecontrol/exons_400bp.noblast.hier'
-#ml java/1.8.0_212
-#popte2='/groups/nordborg/projects/suecica/005scripts/001Software/popte2-v1.10.04.jar'
-#ml java/1.8.0_212
-#TMP='/scratch-cbe/users/robin.burns/tmp'
-#mkdir -p $TMP
-#cd $out
-#java -Xmx60g -Djava.io.tmpdir=$TMP -jar ${popte2} ppileup --bam ${bamfile} --map-qual 20 --hier ${hier} --output ${acc}.ppileup.gz
+hier='/location/hier/file'
+ml java/1.8.0_212
+popte2='/groups/nordborg/projects/suecica/005scripts/001Software/popte2-v1.10.04.jar'
+ml java/1.8.0_212
+TMP='/scratch-cbe/users/robin.burns/tmp'
+mkdir -p $TMP
+cd $out
+java -Xmx60g -Djava.io.tmpdir=$TMP -jar ${popte2} ppileup --bam $out/$acc.filt.bam --map-qual 20 --hier ${hier} --output ${acc}.ppileup.gz
 ### identify TE signatures
-#java -Xmx60g -Djava.io.tmpdir=$TMP -jar ${popte2} identifySignatures --ppileup ${acc}.ppileup.gz --mode separate --output ${acc}.signatures --min-count 2
+java -Xmx60g -Djava.io.tmpdir=$TMP -jar ${popte2} identifySignatures --ppileup ${acc}.ppileup.gz --mode separate --output ${acc}.signatures --min-count 2
 ## calculate frequencies
-#java -Xmx60g -Djava.io.tmpdir=$TMP -jar ${popte2} frequency --ppileup ${acc}.ppileup.gz --signature ${acc}.signatures --output ${acc}.freqsig
+java -Xmx60g -Djava.io.tmpdir=$TMP -jar ${popte2} frequency --ppileup ${acc}.ppileup.gz --signature ${acc}.signatures --output ${acc}.freqsig
 ## write TE output 
-#java -Xmx60g -Djava.io.tmpdir=$TMP -jar ${popte2} pairupSignatures --signature ${acc}.freqsig --ref-genome $refgenome --hier ${hier} --min-distance -100 --max-distance 500 --output ${acc}.teinsertions  
-
-#ml r/3.6.2-foss-2018b
-#scripts='/groups/nordborg/projects/transposons/Robin/001TransposableElements/002scripts'
-#R --vanilla --slave --no-save --no-restore --args $out/${bamfile} < ${scripts}/get_genome_coverage.R
+java -Xmx60g -Djava.io.tmpdir=$TMP -jar ${popte2} pairupSignatures --signature ${acc}.freqsig --ref-genome $refgenome --hier ${hier} --min-distance -100 --max-distance 500 --output ${acc}.teinsertions  
 
 
 
@@ -97,27 +93,6 @@ samtools view -h $out/$acc.discF2.bam | awk -F ' ' '$3~"Chr" && $7~"exon" || $3~
 
 samtools index $out/$acc.discF2_chrexon.bam
 
-
-#ml picard/2.18.27-java-1.8
-#java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
-#	             I=$out/$acc.discF2_chrexon.bam \
-#	             O=$out/$acc.discF2_chrexon_pgmd.bam \
-#	             M=$out/${acc}_dup_metrics_ugh.txt \
-#		     REMOVE_DUPLICATES=true
-
-#mv $out/$acc.discF2_chrexon_pgmd.bam $out/$acc.discF2_chrexon.bam
-#samtools index $out/$acc.discF2_chrexon.bam
-
-
-
-#java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
-#	I=$out/$acc.filt_properpairs.bam \
-#	O=$out/$acc.filt_properpairs_pgmd.bam \
-#	M=$out/${acc}_dup_metrics_ugh2.txt \
-#	REMOVE_DUPLICATES=true
-
-#mv $out/$acc.filt_properpairs_pgmd.bam $out/$acc.filt_properpairs.bam
-#samtools index $out/$acc.filt_properpairs.bam
 
 cd $out
 ml gcc/8.3.0
