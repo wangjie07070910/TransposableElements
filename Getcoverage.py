7d932e9c4867ed838eea5b80aa7c49b05a2c35af
Robin Burns <burnsro@tcd.ie>
2023-02-04 19:58:41 +0000

    Update Getcoverage.py

--- Getcoverage.py
+++ Getcoverage.py
@@ -7,5 +7,10 @@ bam_files = [f for f in os.listdir(directory) if f.endswith("rmdup.bam")]
 for bam_file in bam_files:
     bam_path = os.path.join(directory, bam_file)
     bam = pysam.AlignmentFile(bam_path, "rb")
-    mean_coverage = sum(bam.count_coverage(bam.getrname(i)) for i in range(bam.nreferences)) / bam.nreferences
+    coverage_sum = 0
+    for i in range(bam.nreferences):
+        ref = bam.getrname(i)
+        coverage = bam.count_coverage(ref)
+        coverage_sum += sum(coverage)
+    mean_coverage = coverage_sum / bam.nreferences / bam.lengths[i]
     print("Mean coverage of {}: {:.2f}".format(bam_file, mean_coverage))
