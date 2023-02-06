71d22307625e42af8a91e65bd487ac508ebe0d14
Robin Burns <burnsro@tcd.ie>
2023-02-06 14:24:33 +0000

    Update Get_discordant_reads.py

--- Get_discordant_reads.py
+++ Get_discordant_reads.py
@@ -1,29 +1,31 @@
 import pysam
 import argparse
-import pandas as pd
+import csv
 
-# Define and parse command line arguments
-parser = argparse.ArgumentParser(description='Extract discordant reads from bam file')
-parser.add_argument('-b', '--bamfile', help='Input bam file', required=True)
-parser.add_argument('-o', '--outputfile', help='Output bam file', required=True)
-parser.add_argument('-h', '--hierfile', help='Family order file', required=True)
-args = parser.parse_args()
+def extract_discordant(input_bam, hier_file, output_bam):
+    bamfile = pysam.AlignmentFile(input_bam, "rb")
+    chrs = ['6244_Chr1', '6244_Chr2', '6244_Chr3', '6244_Chr4', '6244_Chr5']
+    id_set = set()
+    with open(hier_file, "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            id_set.add(row[0])
 
-# Open input bam file
-bamfile = pysam.AlignmentFile(args.bamfile, 'rb')
+    outbam = pysam.AlignmentFile(output_bam, "wb", template=bamfile)
 
-# Read family order file into dataframe
-df = pd.read_csv(args.hierfile)
-id_list = df['id'].tolist()
+    for read in bamfile.fetch():
+        if read.is_paired:
+            if (read.reference_name in chrs) != (read.next_reference_name in id_set):
+                outbam.write(read)
 
-# Open output bam file
-outputfile = pysam.AlignmentFile(args.outputfile, 'wb', header=bamfile.header)
+    outbam.close()
+    bamfile.close()
 
-# Extract discordant reads and write to output bam file
-for read in bamfile.fetch():
-    if (read.is_proper_pair) == False and ((read.reference_name in id_list) != (read.next_reference_name in id_list)):
-        outputfile.write(read)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract discordant reads")
+    parser.add_argument("-b", "--bamfile", type=str, required=True, help="Input bam file")
+    parser.add_argument("-h", "--hierfile", type=str, required=True, help="Family order csv file")
+    parser.add_argument("-o", "--outfile", type=str, required=True, help="Output bam file")
+    args = parser.parse_args()
 
-# Close input and output bam files
-bamfile.close()
-outputfile.close()
+    extract_discordant(args.bamfile, args.hierfile, args.outfile)
