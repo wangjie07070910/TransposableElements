fd203962c813fb1db4decbf4d7977e19dc8186a5
Robin Burns <burnsro@tcd.ie>
2023-02-07 14:49:28 +0000

    Update Step4_check_chimeric_reads.py

--- Step4_check_chimeric_reads.py
+++ Step4_check_chimeric_reads.py
@@ -18,46 +18,47 @@ hierarchy_df = pd.read_csv(args.hierarchy, sep='\t')
 # Set the column names for matrix_df
 matrix_df.columns = ['sample_chr', 'start', 'end', 'gene', 'value']
 
+# Create lists to store the overlapped and non-overlapped rows
+myoverlap1 = []
+myoverlap2 = []
+
 # Open the bam file using pysam
 bam_file = pysam.AlignmentFile(args.bam, "rb")
 
-# Loop through the bam file
-for read in bam_file.fetch():
-    #print("Processing read")
-    #print(read)
-    #break
-    # Check if the chromosome matches
-    if read.reference_name == matrix_df.loc[0, 'sample_chr']:
-        #print(read.reference_start)
-        #print(matrix_df.loc[2,'start'])
-        #print(abs(read.reference_start - matrix_df.loc[4,'start']))
-        #break
-        # Check if the start of the read is within 1kb of col2 of the matrix file
-        #if np.any(np.abs(read.reference_start - matrix_df.loc[matrix_df['sample_chr'] == bam_file.get_reference_name(read.reference_id), 'start'].iloc[0]) <= 1000):
-        match = np.abs(read.reference_start - matrix_df.loc[,'start']) <= 2000
-        if np.any(match):
+# Loop through the matrix file
+for i, row in matrix_df.iterrows():
+    # Check if the chromosome of the read matches the chromosome of the current row
+    if row['sample_chr'] in bam_file.references:
+        # Find all reads that match the chromosome and are within 1kb of the start of the current row
+        overlapping_reads = bam_file.fetch(row['sample_chr'], row['start'] - 1000, row['start'] + 1000)
+        overlap = False
+        # Loop through the overlapping reads
+        for read in overlapping_reads:
             # Get the XA or SA tag
             for tag in read.get_tags():
-                print("Found a tag")
-                #print(tag)
-                #break
                 if tag[0] == 'XA' or tag[0] == 'SA':
                     gene = tag[1].split(',')[0]
-                    print(gene)
-                    # If there is a string that matches column1 of the hierarchy file
-                    #if gene in hierarchy_df['id'].values:
+                    # If there is a matching gene in the hierarchy file
                     if hierarchy_df[hierarchy_df['id'].str.contains(gene)].shape[0] > 0:
                         hierarchy_gene = hierarchy_df.loc[hierarchy_df['id'] == gene, 'family'].iloc[0]
-                        # If Column 2 of the hierarchy file matches col4 in the matrix file
-                        if hierarchy_gene in matrix_df['gene'].values:
-                            matrix_index = matrix_df.index[matrix_df['gene'] == hierarchy_gene].tolist()[0]
-                            # If column5 of the matrix file is 0 or NA, change this to 1
-                            if matrix_df.loc[matrix_index, 'value'] in [0, 'NA']:
-                                matrix_df.at[matrix_index, 'value'] = 1
+                        # If the hierarchy gene matches the gene of the current row
+                        if hierarchy_gene == row['gene']:
+                            overlap = True
+                            break
+            if overlap:
+                break
+        # Store the current row in the appropriate list and modify the value of column 5 if necessary
+        if overlap:
+            if row['value'] in [0, 'NA']:
+                row['value'] = 1
+            myoverlap1.append(row)
+        else:
+            myoverlap2.append(row)
 
 # Close the bam file
 bam_file.close()
 
-# Write the output to a file
-matrix_df.to_csv(args.output, sep=',', index=False)
+# Write the overlapped and non-overlapped rows to separate files
+pd.DataFrame(myoverlap1).to_csv(args.output + "_overlap1.csv", index=False)
+pd.DataFrame(myoverlap2).to_csv(args.output + "_overlap2.csv", index=False)
 
